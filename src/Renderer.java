27b59ed696e9ad5cc9c0d4cfc9ac6bc83e8881cf
yatu360 <yatu360@gmail.com>
2021-05-31 23:33:50 +0100

    Jframe setup

---
+++ src/Renderer.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Renderer extends JPanel {
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        flappyBirds.flappyBird.repaint(g);
+    }
+}
